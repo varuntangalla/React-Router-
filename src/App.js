d804647eb4102c1694ba09e6956d754d34544c13
stangalla <stangalla@miraclesoft.com>
2020-05-22 13:34:20 -0400

    Implementing React Router Concepts

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { 
+  Route,
+  Link,
+  BrowserRouter as Router,
+} from 'react-router-dom'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const Home = () => (
+  <div> 
+    <h2>Home</h2>
+  </div>
+)
+
+const About = () => (
+  <div> 
+    <h2>About</h2>
+  </div>
+)
+
+
+const Topics = () => (
+  <div> 
+    <h2>Topics</h2>
+  </div>
+)
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div>
+          <ul> 
+            <li><Link to='/'>Home</Link></li>
+            <li><Link to='/about'>About</Link></li>
+            <li><Link to='/topics'>Topics</Link></li>
+          </ul>
+
+          <br /> 
+
+          <Route exact  path='/'  component={Home} /> 
+          <Route path='/about' component={About} />
+          <Route path='/topics' component={Topics} /> 
+        </div>
+      </Router>
+    )
+  }
 }
 
 export default App;
