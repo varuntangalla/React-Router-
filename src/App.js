bdf3b371f01332f33bfa60bfa4f949bf7b1da37e
stangalla <stangalla@miraclesoft.com>
2020-05-22 14:24:03 -0400

    React Router Setup

--- src/App.js
+++ src/App.js
@@ -17,10 +17,42 @@ const About = () => (
   </div>
 )
 
+const  Topic = ({ match }) => (
 
-const Topics = () => (
+  <div>
+    <h3>{match.params.topicId}</h3>
+  </div>
+
+)
+
+const Topics = ({ match }) => (
   <div> 
     <h2>Topics</h2>
+    <ul>
+      <li>
+        <Link to={`${match.url}/rendering`}>
+          Rendering with React
+        </Link>
+      </li>
+      <li>
+        <Link to={`${match.url}/components`}>
+          Components
+        </Link>
+      </li>
+      <li>
+        <Link to={`${match.url}/props-v-state`}> 
+          Props v. State
+        </Link>
+      </li>
+    </ul>
+
+    <Route path='/topics/:topicId' component={Topic} /> 
+
+    <Route exact  path={match.url} render={() => (
+
+      <h3>Please select a topic</h3>
+
+    )} />
   </div>
 )
 
