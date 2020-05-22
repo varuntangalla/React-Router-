8d86bbc6a1c4a6e3442f16ff193b10971f82a0ae
stangalla <stangalla@miraclesoft.com>
2020-05-22 14:46:55 -0400

    Url Parameters

--- src/App.js
+++ src/App.js
@@ -5,54 +5,10 @@ import {
   BrowserRouter as Router,
 } from 'react-router-dom'
 
-const Home = () => (
+
+const Child = ({ match }) => console.log('match', match) || (
   <div> 
-    <h2>Home</h2>
-  </div>
-)
-
-const About = () => (
-  <div> 
-    <h2>About</h2>
-  </div>
-)
-
-const  Topic = ({ match }) => (
-
-  <div>
-    <h3>{match.params.topicId}</h3>
-  </div>
-
-)
-
-const Topics = ({ match }) => (
-  <div> 
-    <h2>Topics</h2>
-    <ul>
-      <li>
-        <Link to={`${match.url}/rendering`}>
-          Rendering with React
-        </Link>
-      </li>
-      <li>
-        <Link to={`${match.url}/components`}>
-          Components
-        </Link>
-      </li>
-      <li>
-        <Link to={`${match.url}/props-v-state`}> 
-          Props v. State
-        </Link>
-      </li>
-    </ul>
-
-    <Route path='/topics/:topicId' component={Topic} /> 
-
-    <Route exact  path={match.url} render={() => (
-
-      <h3>Please select a topic</h3>
-
-    )} />
+    <h3>ID: {match.params.id} </h3>
   </div>
 )
 
@@ -60,18 +16,16 @@ class App extends Component {
   render() {
     return (
       <Router>
-        <div>
-          <ul> 
-            <li><Link to='/'>Home</Link></li>
-            <li><Link to='/about'>About</Link></li>
-            <li><Link to='/topics'>Topics</Link></li>
+        <div> 
+          <h2>Accounts </h2>
+          <ul>
+            <li><Link to='/netflix'>Netflix</Link></li>
+            <li><Link to='/zillow-group'>Zillow Group</Link></li>
+            <li><Link to='/yahoo'>Yahoo</Link></li>
+            <li><Link to='/modus-create'>Modus Create</Link></li>
           </ul>
 
-          <br /> 
-
-          <Route exact  path='/'  component={Home} /> 
-          <Route path='/about' component={About} />
-          <Route path='/topics' component={Topics} /> 
+          <Route path='/:id' component={Child} /> 
         </div>
       </Router>
     )
